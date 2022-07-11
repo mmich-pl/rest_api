382a908a364b15e9aa587225011a44de81c0711d
mmich-pl <m.michal.parzych@gmail.com>
2022-07-11 16:08:21 +0200

    add endpoint that allow register new user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import NewUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = NewUser
+    search_fields = ('email', 'user_name', 'first_name', 'last_name')
+    list_filter = ('email', 'user_name',
+                   'first_name', 'last_name', 'is_active', 'is_staff')
+    ordering = ('-date_joined', 'email')
+    list_display = ('email', 'id', 'user_name', 'first_name', 'last_name',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('user_name', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'last_name',
+                       'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(NewUser, UserAdminConfig)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
 class CustomAccountManager(BaseUserManager):
     def create_superuser(self, email, user_name, first_name, password, **other_fields):
-        other_fields.setdefault('is_satff', True)
+        other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
 
@@ -21,7 +21,7 @@ class CustomAccountManager(BaseUserManager):
     def create_user(self, email, user_name, first_name, password, **other_fields):
 
         if not email:
-            raise ValueError(gettext_lazy('You must provide an email address'))
+            raise ValueError(_('You must provide an email address'))
 
         email = self.normalize_email(email)
         user = self.model(email=email, user_name=user_name,
@@ -32,11 +32,12 @@ class CustomAccountManager(BaseUserManager):
 
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(gettext_lazy('email address'), unique=True)
+    email = models.EmailField(_('email address'), unique=True)
     user_name = models.CharField(max_length=150, unique=True)
     first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    about = models.TextField(gettext_lazy(
+    about = models.TextField(_(
         'about'), max_length=500, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
@@ -44,7 +45,10 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+    REQUIRED_FIELDS = ['user_name', 'first_name', 'last_name']
 
     def __str__(self):
         return self.user_name
+
+    class Meta:
+        verbose_name_plural = 'Users'
